670b11caa1f4683cd9479b9fc67dcd2d4e7e8c05
geeteshmehuria <geeteshmaihuria@gmail.com>
2024-02-27 20:48:11 +0530

    backend

--- controller/notesControllers.js
+++ controller/notesControllers.js
@@ -14,7 +14,6 @@ const createNote = async (req, res) => {
 const getNotes = async (req, res) => {
   try {
     const notes = await NoteModule.find({ userId: req.body.userId });
-    //console.log('notes',notes)
     res.send({ notes });
   } catch (error) {
     console.log(`Error in getting all Notes : ${error}`);
